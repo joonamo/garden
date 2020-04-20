34614bf5503ed0ed1cd22f7e0bb96445676a96c6
Joona Heinikoski <joona.heinikoski@reaktor.com>
2020-04-20 09:16:24 +0300

    Let's try github pages

---
+++ docs/precache-manifest.003276553d4da0e3f5578c05132f390f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ee6a64ce1617ca0c47c81eb2d4d5b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef353963bbabe2347121",
+    "url": "/static/css/main.b4838950.chunk.css"
+  },
+  {
+    "revision": "0c5a8363c1be8d103cb1",
+    "url": "/static/js/2.dd5a52ef.chunk.js"
+  },
+  {
+    "revision": "c6e5fd00bd7773aa450ee173316601fc",
+    "url": "/static/js/2.dd5a52ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef353963bbabe2347121",
+    "url": "/static/js/main.4bb4c7d2.chunk.js"
+  },
+  {
+    "revision": "8ba143cc9f90c38ff0c2",
+    "url": "/static/js/runtime-main.eb2e842f.js"
+  }
+]);
